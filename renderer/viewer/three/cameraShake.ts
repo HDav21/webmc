c5f4c1a0b14228a0502987df55264e89f3d9e0cc
Pranay Prakash <pranay.gp@gmail.com>
2025-03-26 18:19:03 -0700

    fix camera tween

--- renderer/viewer/three/cameraShake.ts
+++ renderer/viewer/three/cameraShake.ts
@@ -1,9 +1,14 @@
 import * as THREE from 'three'
+import * as tweenJs from '@tweenjs/tween.js'
 
 export class CameraShake {
+  // TODO: temporarily disabled roll animation functionality
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly
   private rollAngle = 0
   private get damageRollAmount () { return 5 }
   private get damageAnimDuration () { return 200 }
+  // TODO: temporarily disabled roll animation functionality
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly
   private rollAnimation?: { startTime: number, startRoll: number, targetRoll: number, duration: number, returnToZero?: boolean }
   private basePitch = 0
   private baseYaw = 0
@@ -22,45 +27,48 @@ export class CameraShake {
 
   shakeFromDamage (yaw?: number) {
     // Add roll animation
-    const startRoll = this.rollAngle
-    const targetRoll = startRoll + (yaw ?? (Math.random() < 0.5 ? -1 : 1)) * this.damageRollAmount
+    // const startRoll = this.rollAngle
+    // const targetRoll = startRoll + (yaw ?? (Math.random() < 0.5 ? -1 : 1)) * this.damageRollAmount
 
-    this.rollAnimation = {
-      startTime: performance.now(),
-      startRoll,
-      targetRoll,
-      duration: this.damageAnimDuration / 2
-    }
+    // this.rollAnimation = {
+    //   startTime: performance.now(),
+    //   startRoll,
+    //   targetRoll,
+    //   duration: this.damageAnimDuration / 2
+    // }
   }
 
   update () {
-    // Update roll animation
-    if (this.rollAnimation) {
-      const now = performance.now()
-      const elapsed = now - this.rollAnimation.startTime
-      const progress = Math.min(elapsed / this.rollAnimation.duration, 1)
+    // TODO: to enable shake from damage, uncomment the following roll animation code.
+    // It still needs to be updated to support tweening.
 
-      if (this.rollAnimation.returnToZero) {
-        // Ease back to zero
-        this.rollAngle = this.rollAnimation.startRoll * (1 - this.easeInOut(progress))
-        if (progress === 1) {
-          this.rollAnimation = undefined
-        }
-      } else {
-        // Initial roll
-        this.rollAngle = this.rollAnimation.startRoll + (this.rollAnimation.targetRoll - this.rollAnimation.startRoll) * this.easeOut(progress)
-        if (progress === 1) {
-          // Start return to zero animation
-          this.rollAnimation = {
-            startTime: now,
-            startRoll: this.rollAngle,
-            targetRoll: 0,
-            duration: this.damageAnimDuration / 2,
-            returnToZero: true
-          }
-        }
-      }
-    }
+    // Update roll animation
+    // if (this.rollAnimation) {
+    //   const now = performance.now()
+    //   const elapsed = now - this.rollAnimation.startTime
+    //   const progress = Math.min(elapsed / this.rollAnimation.duration, 1)
+
+    //   if (this.rollAnimation.returnToZero) {
+    //     // Ease back to zero
+    //     this.rollAngle = this.rollAnimation.startRoll * (1 - this.easeInOut(progress))
+    //     if (progress === 1) {
+    //       this.rollAnimation = undefined
+    //     }
+    //   } else {
+    //     // Initial roll
+    //     this.rollAngle = this.rollAnimation.startRoll + (this.rollAnimation.targetRoll - this.rollAnimation.startRoll) * this.easeOut(progress)
+    //     if (progress === 1) {
+    //       // Start return to zero animation
+    //       this.rollAnimation = {
+    //         startTime: now,
+    //         startRoll: this.rollAngle,
+    //         targetRoll: 0,
+    //         duration: this.damageAnimDuration / 2,
+    //         returnToZero: true
+    //       }
+    //     }
+    //   }
+    // }
 
     // Create rotation quaternions
     const pitchQuat = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), this.basePitch)
@@ -69,7 +77,15 @@ export class CameraShake {
 
     // Combine rotations in the correct order: pitch -> yaw -> roll
     const finalQuat = yawQuat.multiply(pitchQuat).multiply(rollQuat)
-    this.camera.setRotationFromQuaternion(finalQuat)
+
+    const rotationObj = { t: 0 }
+    new tweenJs.Tween(rotationObj)
+      .to({ t: 1 }, 100) // 100ms tween
+      .onUpdate(() => {
+        // Use quaternion slerp for smooth interpolation
+        this.camera.quaternion.slerp(finalQuat, rotationObj.t)
+      })
+      .start()
   }
 
   private easeOut (t: number): number {
