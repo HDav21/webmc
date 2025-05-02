6bf3d9d51101449fc9c31eb3cac46f6f8bf79183
nikandrovaelena93@gmail.com <ashcat2507@gmail.com>
2025-05-02 15:23:14 +0700

    fix: corrected scroll of main component at client mods; feat: improoved ux

--- src/react/ModsPage.tsx
+++ src/react/ModsPage.tsx
@@ -211,7 +211,7 @@ const ModSidebar = ({ mod }: { mod: (ModsData['repos'][0]['packages'][0] & { rep
                 return [field.field, {
                   type: 'button' as const,
                   label: field.label,
-                  onButtonClick () {
+                  onButtonClick() {
                     setEditingField({
                       name: field.field,
                       content: field.getContent?.() || mod.installed![field.field] || '',
